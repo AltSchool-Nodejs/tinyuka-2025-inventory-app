7fe5f96be3a9c7d9b572b7d63b2e0e605975ba72
Daniel Adesoji <sojidaniels@gmail.com>
2026-09-11 19:28:58 +0100

    add size

--- items/items.service.js
+++ items/items.service.js
@@ -7,7 +7,8 @@ const createItem = async (item) => {
     const newItem = await ItemModel.create({
         name: item.name,
         quantity: item.quantity,
-        exp_date: item.exp_date
+        exp_date: item.exp_date,
+        size: 'any'
     })
 
     return newItem;
